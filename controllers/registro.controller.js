f8f0779d3c641a211462850eed3ac3def183af2a
Diego Rey <holacinema@hotmail.com>
2022-12-26 11:02:39 -0300

    Se cambio a async registro.controller

--- controllers/registro.controller.js
+++ controllers/registro.controller.js
@@ -1,12 +1,17 @@
 import { clientServices } from "../service/client-service.js";
 
 const formulario = document.querySelector("[data-form]");
-formulario.addEventListener("submit", (evento) => {
+formulario.addEventListener("submit", async (evento) => {
     evento.preventDefault();
     const nombre = document.querySelector("[data-nombre]").value;
     const email = document.querySelector("[data-email]").value;
-    clientServices.crearCliente(nombre, email).then(() => {
+    try{
+        await clientServices.crearCliente(nombre, email)
         //Redirecciona luego de crear cliente
         window.location.href = "/screens/registro_completado.html";
-    }).catch(err => console.log(err));
+    }catch(err){
+        alert("Hubo un error");
+        console.log("Error", err);
+        window.location.href = "/screens/error.html";
+    }
 });
